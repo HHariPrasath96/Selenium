2aa247cee22cc590f5b3d895aebfd4dc9993c157
HHariprasath96 <hariprasathalagesan@gmail.com>
2022-11-09 12:30:05 +0530

    test1

---
+++ src/org/test/Test1.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class Test1 {
+	
+	
+	private void method1() {
+        System.out.println("c#");
+	}
+	
+	
+	private void method2() {
+    System.out.println("selenium");
+	}
+	
+	
+	private void method3() {
+         System.out.println("testing");
+	}
+
+}
